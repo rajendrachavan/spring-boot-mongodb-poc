fbf4a40662098031a243603b3a202db135d79daf
rajendrachavan <rajendrachavan.jvm@gmail.com>
2020-06-25 15:49:55 +0530

    Intial commit (Local).

---
+++ src/main/java/com/neo/springboot_mongodb_poc/SpringBootMongodbPocApplication.java
@@ -0,0 +1,13 @@
+package com.neo.springboot_mongodb_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbPocApplication.class, args);
+	}
+
+}
